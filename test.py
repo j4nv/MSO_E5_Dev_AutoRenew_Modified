388a662cd221acaf02e2062b7ab58a4a1ef57486
j4nv <585472+j4nv@users.noreply.github.com>
2023-05-07 21:23:52 +0200

    Update test.py

--- test.py
+++ test.py
@@ -15,18 +15,17 @@ import json,sys,time
 path=sys.path[0]+r'/Secret.txt'
 num1 = 0
 
-def gettoken(refresh_token):
+def gettoken():
     headers={'Content-Type':'application/x-www-form-urlencoded'
             }
-    data={'grant_type': 'refresh_token',
-          'refresh_token': refresh_token,
+    data={'grant_type': 'client_credentials',
           'client_id':id,
           'client_secret':secret,
-          'redirect_uri':'http://localhost:53682/'
+          'redirect_uri':'http://localhost:53682/',
+          'scope':'https://graph.microsoft.com/.default'
          }
-    html = req.post('https://login.microsoftonline.com/common/oauth2/v2.0/token',data=data,headers=headers)
+    html = req.post('https://login.microsoftonline.com/%s/oauth2/v2.0/token' % (tenant),data=data,headers=headers)
     jsontxt = json.loads(html.text)
-    refresh_token = jsontxt['refresh_token']
     access_token = jsontxt['access_token']
     with open(path, 'w+') as f:
         f.write(refresh_token)
@@ -37,7 +36,7 @@ def main():
     fo.close()
     global num1
     localtime = time.asctime( time.localtime(time.time()) )
-    access_token=gettoken(refresh_token)
+    access_token=gettoken()
     headers={
     'Authorization':access_token,
     'Content-Type':'application/json'
